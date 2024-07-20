efa9a148175036ad014132617e07501f8af97451
Irineu-Vilola <irineumanuelvilda@gmail.com>
2024-07-20 13:57:24 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/**
+   Preload toda configuração do preload.
+   */
+   const preloader = document.querySelector('#preloader');
+   if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+   }
+   
+
+   /**
+   Menu Hamburguer
+   */
+
+   const hamburguer = document.querySelector(".hamburguer");
+    const navmenu = document.querySelector(".nav-menu");
+
+    hamburguer.addEventListener("click", () => {
+
+    hamburguer.classList.toggle('active');
+    navmenu.classList.toggle('active');
+
+    })

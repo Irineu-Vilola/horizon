e0b4518583c6427bc4cd79065d2b9391e1ecf5d7
Irineu-Vilola <irineumanuelvilda@gmail.com>
2024-08-04 15:13:10 +0100

    azar

--- js/main.js
+++ js/main.js
@@ -24,4 +24,29 @@
     })
 
 
+
+    // Obter os elementos
+const modal = document.getElementById('myModal');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeBtn = document.getElementsByClassName('close')[0];
+
+// Abrir a modal
+openModalBtn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+// Fechar a modal
+closeBtn.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// Fechar a modal ao clicar fora dela
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
+
    
